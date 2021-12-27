9313f31ec2fdb90c2ec9b5c0f7008e0252d3c3cf
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-12-27 15:36:19 -0500

    add MUI5

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ErrorIcon from '@mui/icons-material/Error'
+
+export default function NotFound() {
+  return (
+    <div>
+      <ErrorIcon />
+      <p>Pagina no encontrada</p>
+    </div>
+  )
+}
