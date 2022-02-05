fbd5c38b9d59c28cbb9610985b335ed470ef6845
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-02-04 23:57:51 -0500

    add active page styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar(props) {
   const rutas = {
     indice: {
       ruta: '/',
-      etiqueta: 'Index'
+      etiqueta: 'diluminar'
     },
     productos: {
       ruta: '/Productos/',
@@ -26,18 +26,39 @@ export default function NavBar(props) {
 
   const activo = {
     color: '#F2F2F2',
-    textDecoration: 'none',
-    fontWeight: '700'
+    textDecoration: 'none'
   }
-  const inactivo = { color: 'inherit', textDecoration: 'none' }
+
+  const inactivo = {
+    color: '#CCCCCC',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#F2F2F2'
+    }
+  }
 
   return (
     <div>
       <Box sx={{ ...barraPrincipal }}>
-        <Box sx={{ ...links }}>
-          <Link to={indice.ruta} activeStyle={activo} style={inactivo}>
+        {/* Logo que al presionarlo lleva al index */}
+        <Box sx={{ ...logo }}>
+          <Link
+            to={indice.ruta}
+            activeStyle={{ ...activo }}
+            style={{ ...inactivo }}
+          >
             {indice.etiqueta}
           </Link>
+        </Box>
+        {/* Contenedor con enlaces */}
+        <Box sx={{ ...links }}>
+          <Box>
+            <Link to={productos.ruta} activeStyle={activo} style={inactivo}>
+              {productos.etiqueta}
+            </Link>
+            <Box sx={{ ...unoPrueba }} />
+          </Box>
+
           <Link to={productos.ruta} activeStyle={activo} style={inactivo}>
             {productos.etiqueta}
           </Link>
@@ -53,17 +74,46 @@ export default function NavBar(props) {
   )
 }
 
-const links = {
+const unoPrueba = {
+  background: 'rgba(175, 40, 162, 1)',
+  border: '1px solid rgba(175, 40, 162, 1)',
+  boxShadow: '0px 0px 7px 4px rgba(175, 40, 162, 0.25)',
+  boxSizing: 'border-box',
+  width: '100%',
+  height: '3px',
+  borderRadius: '4px'
+}
+
+const logo = {
   fontFamily: `'Baloo 2'`,
-  fontStyle: 'normal',
   fontWeight: 600,
+  fontSize: { xs: '21.3px', sm: '43px' },
+  marginLeft: '75px',
+  '&:hover': {
+    color: 'yellow'
+  }
+}
+
+const links = {
+  fontFamily: 'Assistant',
+  fontStyle: 'normal',
+  fontWeight: 700,
+  fontSize: '18px',
   display: 'flex',
   flexDirection: 'row',
   gap: 3,
-  color: '#CCCCCC'
+  color: '#CCCCCC',
+  marginRight: '75px'
 }
 
 const barraPrincipal = {
-  height: '3.646vw',
+  display: 'flex',
+  flexDirection: 'row',
+  alignContent: 'center',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexWrap: 'nowrap',
+  height: '68px',
+  width: '100vw',
   background: 'linear-gradient(270deg, #041426 7.34%, #192940 80.71%)'
 }
