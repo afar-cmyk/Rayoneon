68ea9d5c17ea6ff5a417b0e7fbf7ffea4ce9a9f4
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-02-15 21:27:04 -0500

    add main text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,15 @@ export default function index() {
       <NavBar estado={null} />
       <br />
       <p>
-        Esta es la pagina <b>Index</b>
+        <h1>diluminar</h1>
+        <h2>Avisos de Neón</h2>
+        <p>
+          Somos una empresa <b>100% colombiana</b>, dedicada a la
+          <b>fabricación de material publicitario</b> (neón, avisos en acrílico,
+          letras 3D, material p.o.p) Nuestra razón de ser es proporcionarle un
+          servicio puntual y de <b>excelente calidad</b> siempre a tiempo y en
+          el lugar donde nos indique.
+        </p>
       </p>
     </div>
   )
