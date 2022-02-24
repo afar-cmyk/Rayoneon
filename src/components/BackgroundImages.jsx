faa0be51baadd2e2ef51d1a7c4b1c8941b9464ef
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-02-23 23:53:26 -0500

    Moves library component into a react component

--- src/components/BackgroundImages.jsx
+++ src/components/BackgroundImages.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
-import { Box } from '@mui/material'
-import { StaticImage } from 'gatsby-plugin-image'
+import { useStaticQuery, graphql } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
-export default function BackgroundImages() {
+export default function BackgroundImages({ children }) {
+  const { desktop } = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "bck_img001.png" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `
+  )
+  const imageData = desktop.childImageSharp.fluid
   return (
     <>
-      <Box
-        component='div'
-        sx={{
-          backgroundColor: '#041426',
-          width: '100vw',
-          height: '100vh'
-        }}
+      <BackgroundImage
+        Tag='section'
+        // className={className}
+        fluid={imageData}
+        backgroundColor={`#040e18`}
+        style={{ width: '100vw' }}
       >
-        <StaticImage
-          alt='Imagen de fondo'
-          placeholder='blurred'
-          layout='fullWidth'
-          src={'../images/bck_img001.png'}
-        />
-      </Box>
+        {children}
+      </BackgroundImage>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,35 +2,13 @@ import React from 'react'
 import { Box } from '@mui/material'
 import NavBar from '../components/navigation/NavBar'
 import '../styles/global.css'
-// import BackgroundImages from '../components/BackgroundImages.jsx'
-import BackgroundImage from 'gatsby-background-image'
-import { useStaticQuery, graphql } from 'gatsby'
+import BackgroundImages from '../components/BackgroundImages.jsx'
 
 export default function index() {
-  const { desktop } = useStaticQuery(
-    graphql`
-      query {
-        desktop: file(relativePath: { eq: "bck_img001.png" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 4160) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            }
-          }
-        }
-      }
-    `
-  )
-  const imageData = desktop.childImageSharp.fluid
   return (
     <div>
       <NavBar estado={null} />
-      <BackgroundImage
-        Tag='section'
-        // className={className}
-        fluid={imageData}
-        backgroundColor={`#040e18`}
-        style={{ width: '100vw' }}
-      >
+      <BackgroundImages>
         <Box component='div'>
           <h1>diluminar</h1>
           <h2>Avisos de Neón</h2>
@@ -42,7 +20,7 @@ export default function index() {
             siempre a tiempo y en el lugar donde nos indique.
           </p>
         </Box>
-      </BackgroundImage>
+      </BackgroundImages>
     </div>
   )
 }
