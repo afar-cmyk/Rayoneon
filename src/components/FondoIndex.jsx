88382d479bb4e263d36237a832dc8e2910b92e0e
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-03-06 22:25:28 -0500

    adds background image basic component

--- src/components/FondoIndex.jsx
+++ src/components/FondoIndex.jsx
@@ -1,5 +1,29 @@
+import React from 'react'
 import BackgroundSlider from 'react-background-slider'
+import { Box } from '@mui/material'
 import imagen1 from '../images/bck_img001.png'
 import imagen2 from '../images/bck_img002.jpg'
 import imagen3 from '../images/bck_img003.jpg'
-import { Box } from '@mui/material'
+
+export default function FondoIndex({ children }) {
+  return (
+    <>
+      <Box component='div' style={{ ...overlayFondo }}>
+        {children}
+      </Box>
+      <BackgroundSlider
+        style={{
+          ...overlayFondo
+        }}
+        images={[imagen1, imagen2, imagen3]}
+        duration={5}
+        transition={1}
+      />
+    </>
+  )
+}
+
+let overlayFondo = {
+  background: 'linear-gradient(270deg, #041426d5 7.34%, #192940d5 80.71%)',
+  height: '100vh'
+}
