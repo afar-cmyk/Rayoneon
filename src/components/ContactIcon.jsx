a73706b641bc9e1d7fe1b782f01814bd168de0bd
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-03-10 23:04:58 -0500

    adds whatsapp icon

--- src/components/ContactIcon.jsx
+++ src/components/ContactIcon.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import iconoWhatsapp from '../images/whatsapp.svg'
 
 export const ContactIcon = () => {
   return (
     <div>
+      <img alt='Logo whatsapp' href={iconoWhatsapp} />
       <p>¡Contactenos!</p>
     </div>
   )
