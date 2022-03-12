d659113d8ca7ae337164cf26cd780d0adbf3a17c
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-03-12 01:41:19 -0500

    adds props to InlineSVG component

--- src/components/ContactIcon.jsx
+++ src/components/ContactIcon.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import iconoWhatsapp from '../images/whatsapp.svg'
+// import iconoWhatsapp from '../images/whatsapp.svg'
+import InlineSVG from './InlineSVG'
 import '../styles/global.css'
 
 const ContactIcon = () => {
   return (
     <div className='icono-contacto'>
-      <img
+      <InlineSVG icono='whatsapp' />
+      {/* <img
         width='48px'
         id='icono-svg'
         alt='Logo whatsapp'
         src={iconoWhatsapp}
-      />
+      /> */}
       <p>¡Contactenos!</p>
     </div>
   )
