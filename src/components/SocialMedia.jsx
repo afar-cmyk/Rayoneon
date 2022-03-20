38d52c033ed2ce2f2d060720e335b2d5ce9dd136
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-03-19 22:35:12 -0500

    add container for social media icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import PinterestIcon from '@mui/icons-material/Pinterest'
+import YouTubeIcon from '@mui/icons-material/YouTube'
+import EmailIcon from '@mui/icons-material/Email'
+import { Box } from '@mui/material'
 
 const SocialMedia = () => {
-  return <div>SocialMedia</div>
+  return (
+    <>
+      <Box component='div' sx={{ ...cuadroSocialMedia }}></Box>
+    </>
+  )
 }
 
 export default SocialMedia
