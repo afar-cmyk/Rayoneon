4b029e85075b1a921b28403bf7c983eaa2f31e09
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-07 00:25:27 -0500

    renew index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,12 +46,15 @@ const NuevoComponente = () => {
     position: 'relative',
     minHeight: 'calc(100vh - 68px)',
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     clear: 'both',
     zIndex: 0,
-    transition: 'padding .6s ease-in-out'
+    transition: 'padding .6s ease-in-out',
+    gap: '160px',
+    paddingLeft: '96px',
+    paddingRight: '95px'
   }
 
   // let barra = {
@@ -92,7 +95,10 @@ const NuevoComponente = () => {
           <p>
             Somos una empresa <strong>100% colombiana</strong>, dedicada a la{' '}
             <strong>fabricación de material publicitario</strong>
-            {' ('}neón, avisos en acrílico, letras 3D, material p.o.p{') '}
+            {' ('}neón, avisos en acrílico, letras 3D, material p.o.p{').'}
+          </p>
+          <br />
+          <p>
             Nuestra razón de ser es proporcionarle un servicio puntual y de{' '}
             <strong>excelente calidad</strong> siempre a tiempo y en el lugar
             donde nos indique.
