6ae32197e64c62cf29e70cd48fc68b03434a2583
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-07 23:37:23 -0500

    replace test component with main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import NavBar from '../components/navigation/NavBar'
-import FondoIndex from '../components/FondoIndex'
 import '../styles/global.css'
 import ContactIcon from '../components/ContactIcon.jsx'
 import {
@@ -19,7 +18,7 @@ export default function index() {
     <>
       <main id='contenedor-padre'>
         <NavBar estado={null} />
-        <NuevoComponente />
+        <SeccionPrincipal />
         {/* <SeccionPrincipal />
         <div className='seccion-servicios'>
           <ServciosTexto />
@@ -91,7 +90,7 @@ const NuevoComponente = () => {
       <section style={{ ...primero }}>
         <Logo elemento='completo' />
         <div className='contenido-principal'>
-          <h2>Avisos de Neón</h2>
+          <h1>Avisos de Neón</h1>
           <p>
             Somos una empresa <strong>100% colombiana</strong>, dedicada a la{' '}
             <strong>fabricación de material publicitario</strong>
@@ -113,22 +112,23 @@ const NuevoComponente = () => {
 const SeccionPrincipal = () => {
   return (
     <>
-      <FondoIndex>
-        <div className='titulo-principal'>
-          <Logo elemento='completo' />
-        </div>
+      <section className='envoltura-principal'>
+        <Logo elemento='completo' />
         <div className='contenido-principal'>
-          <h2>Avisos de Neón</h2>
+          <h1>Avisos de Neón</h1>
           <p>
             Somos una empresa <strong>100% colombiana</strong>, dedicada a la{' '}
             <strong>fabricación de material publicitario</strong>
-            {' ('}neón, avisos en acrílico, letras 3D, material p.o.p{') '}
+            {' ('}neón, avisos en acrílico, letras 3D, material p.o.p{').'}
+          </p>
+          <br />
+          <p>
             Nuestra razón de ser es proporcionarle un servicio puntual y de{' '}
             <strong>excelente calidad</strong> siempre a tiempo y en el lugar
             donde nos indique.
           </p>
         </div>
-      </FondoIndex>
+      </section>
     </>
   )
 }
