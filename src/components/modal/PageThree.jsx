8c7cd01e4eb8221c6b86276976517c53bff3787c
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-18 23:00:35 -0500

    fix pigeon maps styles

--- src/components/modal/PageThree.jsx
+++ src/components/modal/PageThree.jsx
@@ -5,14 +5,21 @@ const PageThree = () => {
   const coordenadas = [4.623235828150638, -74.0742739482184]
   return (
     <>
-      <p style={{ width: '100%' }}>pagina tres</p>
-      {/* <GoogleMap apiKey='AIzaSyCGGBdvSPWtC-XDBda8hLSiuNBxXpj4RWg'>
-        <Marker position={{ lat: 4.623235828150638, lng: -74.0742739482184 }} />
-      </GoogleMap> */}
-
-      <Map defaultCenter={coordenadas} defaultZoom={19} boxClassname='mapa'>
-        <Marker width={45} anchor={coordenadas} color='red' />
-      </Map>
+      <section className='pagina-tres'>
+        <p>pagina tres</p>
+        <div
+          style={{
+            width: '66vw',
+            height: '35vh',
+            borderRadius: '6px',
+            overflow: 'hidden'
+          }}
+        >
+          <Map defaultCenter={coordenadas} defaultZoom={19}>
+            <Marker width={45} anchor={coordenadas} color='red' />
+          </Map>
+        </div>
+      </section>
     </>
   )
 }
