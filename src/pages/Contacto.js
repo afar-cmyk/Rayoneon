ffd8eaf653abc932451afb0a304725fb8c6b0876
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-20 23:44:32 -0500

    add contact's page base content

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -6,9 +6,28 @@ export default function Contacto() {
     <div>
       <NavBar estado={2} />
       <section className='pagina-contacto'>
-        <p>
-          Esta es la pagina <b>Contacto</b>
-        </p>
+        <div>
+          <h1>¡Contactenos!</h1>
+          <br />
+          <h2>contacto@diluminar.com</h2>
+          <p>
+            para trabajar con nosotros, cotizaciones, materiales y diseños, con
+            gusto le responderemos lo mas pronto posible!
+          </p>
+          <br />
+          <h2>Bogotá D.C</h2>
+          <ul>
+            <li>Ak. 19 # 33a-36, Cundinamarca</li>
+            <li>(+57) 310 881 55 44</li>
+            <li>(601) 332 6290</li>
+          </ul>
+          <br />
+          <h2>Redes sociales</h2>
+          <p>
+            Siguenos para conocer nuestras ultimas noticias, proyectos y
+            promociones.
+          </p>
+        </div>
       </section>
     </div>
   )
