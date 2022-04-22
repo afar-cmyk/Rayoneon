539495af7d5ddc5b9883c1850451faec118cbe48
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-22 00:23:50 -0500

    fix creadits styles

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -17,26 +17,28 @@ export default function Contacto() {
             minHeight: 'calc(100vh - 68px)'
           }}
         >
-          <h1>¡Contactenos!</h1>
-          <br />
-          <h2>contacto@diluminar.com</h2>
-          <p>
-            para trabajar con nosotros, cotizaciones, materiales y diseños, con
-            gusto le responderemos lo mas pronto posible!
-          </p>
-          <br />
-          <h2>Bogotá D.C</h2>
-          <ul>
-            <li>Ak. 19 # 33a-36, Cundinamarca</li>
-            <li>(+57) 310 881 55 44</li>
-            <li>(601) 332 6290</li>
-          </ul>
-          <br />
-          <h2>Redes sociales</h2>
-          <p>
-            Siguenos para conocer nuestras ultimas noticias, proyectos y
-            promociones.
-          </p>
+          <div style={{ marginLeft: '75px' }}>
+            <h1>¡Contactenos!</h1>
+            <br />
+            <h2>contacto@diluminar.com</h2>
+            <p style={{ width: '400px' }}>
+              para trabajar con nosotros, cotizaciones, materiales y diseños,
+              con gusto le responderemos lo mas pronto posible!
+            </p>
+            <br />
+            <h2>Bogotá D.C</h2>
+            <ul>
+              <li>Ak. 19 # 33a-36, Cundinamarca</li>
+              <li>(+57) 310 881 55 44</li>
+              <li>(601) 332 6290</li>
+            </ul>
+            <br />
+            <h2>Redes sociales</h2>
+            <p style={{ width: '400px' }}>
+              Siguenos para conocer nuestras ultimas noticias, proyectos y
+              promociones.
+            </p>
+          </div>
         </div>
         <Footer />
         <video className='video-fondo' autoPlay loop muted src={bck_vid} />
