c6a227ef8a711edd9740d7b284fb30aca4f74e79
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-23 00:21:48 -0500

    add inline socialmedia icons

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -3,6 +3,15 @@ import NavBar from '../components/navigation/NavBar'
 import bck_vid from '../images/bck_vid.webm'
 import { Box, Tooltip } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import {
+  Instagram,
+  Facebook,
+  Reviews,
+  Pinterest,
+  YouTube,
+  Email,
+  LocationOn
+} from '@mui/icons-material'
 
 export default function Contacto() {
   return (
@@ -38,6 +47,7 @@ export default function Contacto() {
               Siguenos para conocer nuestras ultimas noticias, proyectos y
               promociones.
             </p>
+            <SocialMedia />
           </div>
         </div>
         <Footer />
@@ -80,6 +90,68 @@ const IconoGithub = () => {
           <Box component={GitHubIcon} className='iconoMUI' />
         </a>
       </Tooltip>
+    </>
+  )
+}
+
+const SocialMedia = () => {
+  let iconosSocialmedia = [
+    {
+      icono: Instagram,
+      enlace: 'https://www.instagram.com/rayoneon/',
+      clase: 'Instagram'
+    },
+    {
+      icono: Facebook,
+      enlace: 'https://www.facebook.com/Rayo-neon-1400859480129814',
+      clase: 'Facebook'
+    },
+    {
+      icono: YouTube,
+      enlace: 'https://www.youtube.com/watch?v=Qwcv8hzs5Hc',
+      clase: 'Youtube'
+    },
+    {
+      icono: Pinterest,
+      enlace: 'https://es.pinterest.com/rayoneon/pins/',
+      clase: 'Pinterest'
+    },
+    {
+      icono: Reviews,
+      enlace:
+        'https://www.google.com/search?q=rayo+neon&oq=rayo+neon&aqs=chrome..69i57j0i22i30l4j69i60l3.1247j1j4&sourceid=chrome&ie=UTF-8#lrd=0x8e3f999d072db581:0xf451a327625f2cf9,1,,,',
+      clase: 'Reseñas'
+    },
+    {
+      icono: Email,
+      enlace: 'mailto:ventas@rayoneon.com',
+      clase: 'E-mail'
+    },
+    {
+      icono: LocationOn,
+      enlace: 'https://goo.gl/maps/FcHoncUTB9HCkdP1A',
+      clase: 'Ubicación'
+    }
+  ]
+  return (
+    <>
+      <div className='redes-sociales-creditos'>
+        {iconosSocialmedia.map((datos, index) => {
+          const { icono, enlace, clase } = datos
+          return (
+            <Tooltip key={index} title={clase} arrow>
+              <a
+                href={enlace}
+                target='_blank'
+                rel='noreferrer'
+                className={`cuadro-social-media ${clase}`}
+              >
+                <Box component={icono} className='iconoMUI' />
+              </a>
+            </Tooltip>
+          )
+        })}
+      </div>
     </>
   )
 }
