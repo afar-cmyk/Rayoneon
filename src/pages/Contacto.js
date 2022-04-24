830358582910ec41774cb687cd22b7e050b57426
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-23 21:27:14 -0500

    update contact information

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -32,20 +32,20 @@ export default function Contacto() {
           <div style={{ marginLeft: '75px' }}>
             <h1>¡Contactenos!</h1>
             <br />
-            <h2>contacto@diluminar.com</h2>
+            <h2>contacto@rayo-neon.com</h2>
             <p style={{ width: '400px' }}>
               para trabajar con nosotros, cotizaciones, materiales y diseños,
               con gusto le responderemos lo mas pronto posible!
             </p>
             <br />
-            <h2>Bogotá D.C</h2>
+            <h2>Colombia / Bogotá D.C.</h2>
             <ul>
               <li>Ak. 19 # 33a-36, Cundinamarca</li>
               <li>(+57) 310 881 55 44</li>
               <li>(601) 332 6290</li>
             </ul>
             <br />
-            <h2>Redes sociales</h2>
+            <h2>Nuestras redes sociales</h2>
             <p style={{ width: '400px' }}>
               Siguenos para conocer nuestras ultimas noticias, proyectos y
               promociones.
