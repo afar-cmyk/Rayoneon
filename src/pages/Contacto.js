ca8854f88c86999ea6f91e0cb7c5e4c8973a1b7f
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-04-24 23:57:10 -0500

    Testing responsive

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -29,29 +29,43 @@ export default function Contacto() {
             flexDirection: 'row'
           }}
         >
-          <div style={{ marginLeft: '75px' }}>
+          <Box
+            sx={{
+              marginLeft: '75px',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: { lg: '0px', xl: '30px' }
+            }}
+          >
             <h1>¡Contactenos!</h1>
             <br />
-            <h2>contacto@rayo-neon.com</h2>
-            <p style={{ width: '400px' }}>
-              para trabajar con nosotros, cotizaciones, materiales y diseños,
-              con gusto le responderemos lo mas pronto posible!
-            </p>
+            <div>
+              <h2>contacto@rayo-neon.com</h2>
+              <p style={{ width: '400px' }}>
+                para trabajar con nosotros, cotizaciones, materiales y diseños,
+                con gusto le responderemos lo mas pronto posible!
+              </p>
+            </div>
             <br />
-            <h2>Colombia / Bogotá D.C.</h2>
-            <ul>
-              <li>Ak. 19 # 33a-36, Cundinamarca</li>
-              <li>(+57) 310 881 55 44</li>
-              <li>(601) 332 6290</li>
-            </ul>
+            <div>
+              <h2>Colombia / Bogotá D.C.</h2>
+              <ul>
+                <li>Ak. 19 # 33a-36, Cundinamarca</li>
+                <li>(+57) 310 881 55 44</li>
+                <li>(601) 332 6290</li>
+              </ul>
+            </div>
+
             <br />
-            <h2>Nuestras redes sociales</h2>
-            <p style={{ width: '400px' }}>
-              Siguenos para conocer nuestras ultimas noticias, proyectos y
-              promociones.
-            </p>
-            <SocialMedia />
-          </div>
+            <div>
+              <h2>Nuestras redes sociales</h2>
+              <p style={{ width: '400px' }}>
+                Siguenos para conocer nuestras ultimas noticias, proyectos y
+                promociones.
+              </p>
+              <SocialMedia />
+            </div>
+          </Box>
           <div className='logo-creditos'>
             <Logo elemento='completo' />
           </div>
